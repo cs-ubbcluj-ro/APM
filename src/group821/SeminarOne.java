7ab9121987c3d1a0275df9cdbadde936452106e6
ibocicor <ibocicor@gmail.com>
2024-10-10 11:38:47 +0300

    First commit for seminar 1.

--- src/group821/SeminarOne.java
+++ src/group821/SeminarOne.java
@@ -1,7 +1,5 @@
-package group821;
-
 public class SeminarOne {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
